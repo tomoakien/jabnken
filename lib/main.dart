8a40dd97677681d2a45266f7232fc3cc7135d668
tomoaki_narukawa <tomo111911111@gmail.com>
2024-06-04 21:05:56 +0900

    初回コミット

---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String jankenText = '👊';
+
+  void _chooseJankenText() {
+    setState(() {
+      jankenText = '✋';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '相手',
+              style: TextStyle(fontSize: 30),
+            ),
+            Text(
+              '✌️',
+              style: TextStyle(fontSize: 100),
+            ),
+            SizedBox(
+              height: 80,
+            ),
+            Text(
+              '自分',
+              style: TextStyle(fontSize: 30),
+            ),
+            Text(
+              jankenText,
+              style: TextStyle(fontSize: 200),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _chooseJankenText,
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
